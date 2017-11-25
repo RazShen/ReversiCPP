4489f98bbde493bdffa7fe78350aea1cdab57431
tomergrady <tomergrady@gmail.com>
2017-11-25 16:26:20 +0200

    updating commands

--- src/ConsoleDisplay.h
+++ src/ConsoleDisplay.h
@@ -20,24 +20,84 @@ public:
     * @param board given board
      */
     void printBoard(Board* board);
+    /**
+     * this method prints possible Moves
+     * @param given positions
+     * @param moves number of moves
+     */
     void possibleMoves(Pair positions[], int moves);
+    /**
+     * this method prints no valid move option
+     */
     void noValidMove();
+    /**
+     * this method prints no valid option
+     */
     void noValidOption();
+    /**
+     * this method prints a current board.
+     */
     void currentBoard();
+    /**
+     * this method prints it's Your Move for a player.
+     */
     void itsYourMove(Board::Status);
+    /**
+     * this method prints x played.
+     */
     void xPlayed();
+    /**
+     * this method prints o played.
+     */
     void oPlayed();
+    /**
+     * this method prints the welcome menu.
+     */
     void welcome();
+    /**
+     * this method prints and ask an input from the player.
+     */
     void getInput();
+    /**
+     * this method prints no possible moves for player.
+     */
     void noPossibleMoves();
+    /**
+     * this method prints new line.
+     */
     void newLine();
+    /**
+     * this method prints a pair.
+     * @param p a given pair
+     */
     void printPair(Pair p);
+    /**
+     * this method prints game options.
+     */
     void chooseGameOptions();
+    /**
+     * this method prints any Key To Exit Massage.
+     */
     void anyKeyToExitMassage();
+    /**
+     * this method prints black win.
+     */
     void declareBlackWin();
+    /**
+     * this method prints white win.
+     */
     void declareWhiteWIn();
+    /**
+     * this method prints it's a draw.
+     */
     void declareDraw();
+    /**
+     * this method prints that the board it full.
+     */
     void fullBoard();
+    /**
+     * this method prints that there is no more moves for both player.
+     */
     void noMoreMovesForAll();
 
 };
--- src/Display.h
+++ src/Display.h
@@ -9,26 +9,89 @@
 
 class Display {
 public:
-
+    /**
+     * this method prints the board
+     * @param board a given board
+     */
     virtual void printBoard(Board* board) = 0;
+    /**
+     * this method prints possible Moves
+     * @param given positions
+     * @param moves number of moves
+     */
     virtual void possibleMoves(Pair positions[], int moves) = 0;
+    /**
+     * this method prints no valid move option
+     */
     virtual void noValidMove() = 0;
+    /**
+     * this method prints no valid option
+     */
     virtual void noValidOption() = 0;
+    /**
+     * this method prints a current board.
+     */
     virtual void currentBoard() = 0;
+    /**
+     * this method prints it's Your Move for a player.
+     */
     virtual void itsYourMove(Board::Status) = 0;
+    /**
+     * this method prints x played.
+     */
     virtual void xPlayed() = 0;
+    /**
+     * this method prints o played.
+     */
     virtual void oPlayed() = 0;
+    /**
+     * this method prints the welcome menu.
+     */
     virtual void welcome() = 0;
+    /**
+     * this method prints and ask an input from the player.
+     */
     virtual void getInput() = 0;
+    /**
+     * this method prints no possible moves for player.
+     */
     virtual void noPossibleMoves() = 0;
+    /**
+     * this method prints new line.
+     */
     virtual void newLine() = 0;
+    /**
+     * this method prints a pair.
+     * @param p a given pair
+     */
     virtual void printPair(Pair p) = 0;
+    /**
+     * this method prints game options.
+     */
     virtual void chooseGameOptions() = 0;
+    /**
+     * this method prints any Key To Exit Massage.
+     */
     virtual void anyKeyToExitMassage() = 0;
+    /**
+     * this method prints black win.
+     */
     virtual void declareBlackWin() = 0;
+    /**
+     * this method prints white win.
+     */
     virtual void declareWhiteWIn() = 0;
+    /**
+     * this method prints it's a draw.
+     */
     virtual void declareDraw() = 0;
+    /**
+     * this method prints that the board it full.
+     */
     virtual void fullBoard() = 0;
+    /**
+     * this method prints that there is no more moves for both player.
+     */
     virtual void noMoreMovesForAll() = 0;
 
 };
